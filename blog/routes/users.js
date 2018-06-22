e4e7a46731592b36fb989703c069f3ae9461796c
wchshi <1004778046@qq.com>
2018-06-22 11:06:35 +0800

    测试

--- blog/routes/users.js
+++ blog/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/set', function(req, res, next) {
+  // 输出 JSON 格式
+   var response = {
+       "first_name":"MrSSSS",
+       "last_name":"missSSSSSSSSSSSS"
+   };
+   console.log(response);
+   res.jsonp(response)
+   // res.send("nnnnnnnnnnnnnn")  
 });
+
+
+
+
 
 module.exports = router;
